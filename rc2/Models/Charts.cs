a62698a039737690639b20c25bd250dbefaf0387
Simon Pettersson <rickebo@rickebo.com>
2023-05-17 13:07:34 +0200

    Added options and configurability, fixed minor formatting issues

---
+++ rc2/Models/Charts.cs
@@ -0,0 +1,237 @@
+﻿using ApexCharts;
+using rc2.Data;
+
+namespace rc2.Models;
+
+public class Charts
+{
+    public const string MoneyFormat =
+        "function (value, time, opts) {" +
+        "  return value > 1000000" +
+        "    ? (value / 1000000).toFixed(2) + 'M'" +
+        "    : value > 1000 " +
+        "        ? (value / 1000).toFixed(1) + 'k' " +
+        "        : value.toFixed(0)" +
+        "}";
+
+    public const string PercentageFormat =
+        "function (value, time, opts) {" +
+        "  return value.toFixed(1) + '%'" +
+        "}";
+
+    public ApexChart<DataPoint> BalanceChart { get; set; }
+
+    public ApexChartOptions<DataPoint> BalanceOptions { get; } = new()
+    {
+        Yaxis = new List<YAxis>()
+        {
+            new YAxis()
+            {
+                DecimalsInFloat = 0,
+                AxisTicks = new AxisTicks()
+                {
+                },
+                Title = new AxisTitle()
+                {
+                    Text = "Loan balance"
+                },
+                Labels = new YAxisLabels()
+                {
+                    Formatter = MoneyFormat
+                }
+            }
+        },
+        Xaxis = new XAxis()
+        {
+            Type = XAxisType.Numeric,
+            // TickAmount = 10,
+            TickPlacement = TickPlacement.On,
+            Title = new AxisTitle()
+            {
+                Text = "Time (years)"
+            }
+        }
+    };
+
+    public ApexChart<DataPoint> CostsChart { get; set; }
+
+    public ApexChartOptions<DataPoint> CostsOptions { get; } = new()
+    {
+        Yaxis = new List<YAxis>()
+        {
+            new YAxis()
+            {
+                DecimalsInFloat = 0,
+                AxisTicks = new AxisTicks()
+                {
+                },
+                Title = new AxisTitle()
+                {
+                    Text = "Monthly expense"
+                },
+            }
+        },
+        Xaxis = new XAxis()
+        {
+            Type = XAxisType.Numeric,
+            // TickAmount = 10,
+            TickPlacement = TickPlacement.On,
+            Title = new AxisTitle()
+            {
+                Text = "Time (years)"
+            }
+        }
+    };
+
+    public ApexChart<Relation> ExpensesChart { get; set; }
+
+    public ApexChartOptions<Relation> ExpensesOptions { get; } = new()
+    {
+        Yaxis = new List<YAxis>()
+        {
+            new YAxis()
+            {
+                DecimalsInFloat = 0,
+                AxisTicks = new AxisTicks()
+                {
+                },
+                Labels = new YAxisLabels()
+                {
+                    Format = MoneyFormat
+                }
+            }
+        },
+        Xaxis = new XAxis()
+        {
+            Type = XAxisType.Numeric,
+            // TickAmount = 10,
+            TickPlacement = TickPlacement.On
+        }
+    };
+
+
+    public ApexChart<InterestDataPoint> InterestChart { get; set; }
+
+    public ApexChartOptions<InterestDataPoint> InterestOptions { get; } = new()
+    {
+        Yaxis = new List<YAxis>()
+        {
+            new YAxis()
+            {
+                DecimalsInFloat = 0,
+                AxisTicks = new AxisTicks()
+                {
+                },
+                Title = new AxisTitle()
+                {
+                    Text = "Min monthly payment"
+                }
+            }
+        },
+        Xaxis = new XAxis()
+        {
+            Type = XAxisType.Numeric,
+            // TickAmount = 10,
+            DecimalsInFloat = 1,
+            Labels = new XAxisLabels()
+            {
+                Formatter = PercentageFormat
+            },
+            Title = new AxisTitle()
+            {
+                Text = "Interest rate"
+            }
+        }
+    };
+
+
+    public ApexChart<InterestDataPoint> InterestProportionChart { get; set; }
+
+    public ApexChartOptions<InterestDataPoint> InterestProportionOptions { get; } = new()
+    {
+        Yaxis = new List<YAxis>()
+        {
+            new YAxis()
+            {
+                DecimalsInFloat = 0,
+                AxisTicks = new AxisTicks()
+                {
+                },
+                Labels = new()
+                {
+                    Formatter = "function (value, time, formatter) { return value.toFixed(0) + '%' }"
+                },
+                Title = new AxisTitle()
+                {
+                    Text = "Proportion"
+                }
+            }
+        },
+        Xaxis = new XAxis()
+        {
+            Type = XAxisType.Numeric,
+            // TickAmount = 10,
+            DecimalsInFloat = 1,
+            Labels = new XAxisLabels()
+            {
+                Formatter = PercentageFormat
+            },
+            Title = new AxisTitle()
+            {
+                Text = "Interest rate"
+            }
+        }
+    };
+
+
+    public ApexChart<BalanceDataPoint> BalanceCostChart { get; set; }
+
+    public ApexChartOptions<BalanceDataPoint> BalanceCostOptions { get; } = new()
+    {
+        Yaxis = new List<YAxis>()
+        {
+            new YAxis()
+            {
+                DecimalsInFloat = 0,
+                AxisTicks = new AxisTicks()
+                {
+                },
+                Title = new AxisTitle()
+                {
+                    Text = "Max required monthly payment"
+                }
+            }
+        },
+        Xaxis = new XAxis()
+        {
+            Type = XAxisType.Numeric,
+            DecimalsInFloat = 1,
+            Labels = new XAxisLabels()
+            {
+                Formatter = MoneyFormat
+            },
+            Title = new AxisTitle()
+            {
+                Text = "Loan"
+            }
+        }
+    };
+
+    public async Task UpdateAll()
+    {
+        var tasks = new []
+        {
+            BalanceChart?.UpdateSeriesAsync(),
+            CostsChart?.UpdateSeriesAsync(),
+            ExpensesChart?.UpdateSeriesAsync(),
+            InterestChart?.UpdateSeriesAsync(),
+            InterestProportionChart?.UpdateSeriesAsync(),
+            BalanceCostChart?.UpdateSeriesAsync()
+        };
+
+        await Task.WhenAll(tasks
+            .Where(task => task != null)
+            .Select(task => task!)
+        );
+    }
+}
---
+++ rc2/Settings/BindableOptions.cs
@@ -0,0 +1,136 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace rc2.Settings;
+
+public class BindableOptions : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public Func<Options> Options { get; }
+
+    public BindableOptions(Func<Options> options)
+    {
+        Options = options;
+    }
+
+    public void UpdateAll()
+    {
+        var properties = new[]
+        {
+            nameof(InterestPercent),
+            nameof(DepositPercent),
+            nameof(DepreciationPercent),
+            nameof(InflationPercent),
+            nameof(Balance),
+            nameof(Payment),
+            nameof(Fees),
+            nameof(TimeStep),
+            nameof(MinAmortization),
+            nameof(InterestDeduction)
+        };
+        
+        foreach (var property in properties)
+            OnPropertyChanged(property);
+    }
+
+    public double InterestPercent
+    {
+        get => Options().Interest * 100;
+        set => SetValue(
+            nameof(InterestPercent),
+            () => Options().Interest = value / 100
+        );
+    }
+
+    public double DepositPercent
+    {
+        get => Options().Deposit * 100;
+        set => SetValue(
+            nameof(DepositPercent),
+            () => Options().Deposit = value / 100
+        );
+    }
+
+    public double DepreciationPercent
+    {
+        get => Options().Depreciation * 100;
+        set => SetValue(
+            nameof(DepreciationPercent),
+            () => Options().Depreciation = value / 100
+        );
+    }
+
+    public double InflationPercent
+    {
+        get => Options().Inflation * 100;
+        set => SetValue(
+            nameof(InflationPercent),
+            () => Options().Inflation = value / 100
+        );
+    }
+
+    public double Balance
+    {
+        get => Options().Balance;
+        set => SetValue(
+            nameof(Balance),
+            () => Options().Balance = value
+        );
+    }
+
+    public double Payment
+    {
+        get => Options().Payment;
+        set => SetValue(
+            nameof(Payment),
+            () => Options().Payment = value
+        );
+    }
+
+    public double Fees
+    {
+        get => Options().Fees;
+        set => SetValue(
+            nameof(Fees),
+            () => Options().Fees = value
+        );
+    }
+
+    public string TimeStep
+    {
+        get => Options().TimeStep;
+        set => SetValue(
+            nameof(TimeStep),
+            () => Options().TimeStep = value
+        );
+    }
+
+    public string MinAmortization
+    {
+        get => Options().MinAmortization;
+        set => SetValue(
+            nameof(MinAmortization),
+            () => Options().MinAmortization = value
+        );
+    }
+
+    public string InterestDeduction
+    {
+        get => Options().InterestDeduction;
+        set => SetValue(
+            nameof(InterestDeduction), 
+            () => Options().InterestDeduction = value
+            );
+    }
+
+    private void SetValue(string propertyName, Action updater)
+    {
+        updater();
+        OnPropertyChanged(propertyName);
+    }
+    
+    public virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
