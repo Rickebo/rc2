15598966b0437983796fb721e7334c8ad4d563dc
Simon Pettersson <rickebo@rickebo.com>
2023-05-17 14:10:21 +0200

    Fixed docker image issues

--- build-image.sh
+++ build-image.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 docker build -t rc2 -f rc2/Dockerfile .
+docker tag rc2:latest repopummcs2xbx.rickebo.com/rickebo/rc2:latest
+docker push repopummcs2xbx.rickebo.com/rickebo/rc2:latest
